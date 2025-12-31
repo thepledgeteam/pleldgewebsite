5cd3c0c843fa17939a0fabefe1602be696c9469f
thepledgeteam <thepledge68@gmail.com>
2025-12-31 09:52:55 -0500

    Update ContactSection.tsx

--- pledged production/src/components/ContactSection.tsx
+++ pledged production/src/components/ContactSection.tsx
@@ -130,7 +130,20 @@ const ContactSection = () => {
   );
 };
 
+<form action="https://formspree.io/f/mvzgpqyd" method="POST">
+    <label for="name">Name</label><br>
+    <input type="text" id="name" name="name" required><br><br>
+
+    <label for="email">Email</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="message">Message</label><br>
+    <textarea id="message" name="message" rows="5" required></textarea><br><br>
+
+    <button type="submit">Submit</button>
+</form>
 
 export default ContactSection;
+
 
 
